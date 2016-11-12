244f831a2d82efd84f85e23779fac4d0b3901ea0
JeroxFX <thecreeper@live.de>
2016-11-12 23:19:24 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
-echo $(curl -s https://jeroxfx.github.io/llM-repo/updatescript.sh) >> llM-updater.sh
+curl -s https://jeroxfx.github.io/llM-repo/updatescript.sh >> llM-updater.sh
 chmod +x llM-updater.sh 
 echo "The llM-update.sh is placed here. you can now add it to you rc.local file"
 
-sudo mkdir /var/www/html/Monitor
-sudo echo $(curl -s https://jeroxfx.github.io/llM-repo/installer.php.raw) >> /var/www/html/Monitor/install.php
-sudo echo "Now go to http://thispc/Monitor/install.php. WARNING this is going to take ages so don't panic"
+sudo su
+mkdir /var/www/html/Monitor
+curl -s https://jeroxfx.github.io/llM-repo/installer.php.raw >> /var/www/html/Monitor/install.php
+echo "Now go to http://thispc/Monitor/install.php. WARNING this is going to take ages so don't panic"
             
         
